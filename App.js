a6ddcddd2b8a5737abd5de6885f0dda4fadf018c
mtmiranda <mattheus_miranda@hotmail.com>
2021-10-11 10:25:35 -0300

    </App> Adding Header and Timer component with structures

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './src/components/Header';
+
+import styled from 'styled-components/native';
+import Timer from './src/components/Timer';
+import { StatusBar, Text } from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Container>
+            <StatusBar />
+            <Header color="#000">
+                <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Pomodoro App</Text>
+            </Header>
+            <Timer pomodoro={1500} shortTime={300} longTime={900} cycles={3} />
+
+        </Container>
+
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #fff;
+  padding: 40px 0;
+
+`;
+
